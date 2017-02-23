5e3ef7fe7efef6c53537f1f8fa14b7e66ef4eb36
DJ <magicm3rk@gmail.com>
2017-02-22 19:39:27 -0500

    streamlined jquery code for picture center, just noticed problem with navbar and collapsing issues

--- main.js
+++ main.js
@@ -1,29 +1,21 @@
 
-  // function for centering Picture (couldn't figure out how to get boostrap to center vert)
-  function  adjustPicturePadding () {   
-
-        document.getElementById("pictureContainer").style.paddingTop =  "0px"; //set to 0 first to prevent bobbing
-        var textContainer = document.getElementById("textContainer"); 
-        height = textContainer.offsetHeight; //gets height of text box to the right
-
-        var heightPic = document.getElementById("pictureContainer");
-        var height2 = heightPic.offsetHeight; //gets height of picture
-        
-        var adjustedPadding = (height/2) - (height2/2); //eq for centering 
-
-        document.getElementById("pictureContainer").style.paddingTop = adjustedPadding + "px";
+  // function for centering Picture against neighboring col (couldn't figure out how to get boostrap to center vert)
+  function  adjustPicturePadding (target1, target2) {   //target 1 is colum  with picture to be centered, col 2 is neighboring
+        target1.style.paddingTop =  "0px"; //set to 0 first to prevent bobbing
+        target1.style.paddingTop = ((target2.offsetHeight/2) - (target1.offsetHeight/2)) + "px";
   }
   // end centering function
 
-
-
-
 $('document').ready( function () {
-    
-    adjustPicturePadding();
-    window.onresize = adjustPicturePadding;
-/* code from https://css-tricks.com/snippets/jquery/smooth-scrolling/ keeping it here till i can figure out how the hell it works
+  
+    var target1 = document.getElementById("pictureContainer");
+    var target2 = document.getElementById("textContainer"); 
+   
+    adjustPicturePadding(target1, target2);
+    function adjustPicturePaddingClone() { adjustPicturePadding(target1, target2); } //resize event listener wont take arguments
+    window.addEventListener('resize', adjustPicturePaddingClone);
 
+/*
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -37,8 +29,9 @@ $(function() {
       }
     }
   });
-});
-*/
+});*/
+
+
 
 
 });
